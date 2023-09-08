3575d59c52f20dcebb0de25a66407f6292f667fb
DennisAO <dennisaokita@gmail.com>
2023-09-07 19:29:24 -0700

    My first .c file

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main -  this is the main of the file
+ *
+ * Return: always returns 0
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
+}
