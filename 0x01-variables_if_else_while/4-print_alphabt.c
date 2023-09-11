f42400e042c46e091e147bff8356985b9db96474
DennisAO <dennisaokita@gmail.com>
2023-09-11 03:43:12 -0700

    Write a program that prints all possible combinations of single-digit numbers

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,11 +7,15 @@
  */
 int main(void)
 {
-	char a;
+	int i;
 
-	for (a = 'a'; a <= 'z'; a++)
-	for (a != 'e' && a != 'q')
-		putchar(a);
-       	putchar('\n');
+	for (i = 97; i < 123; i++)
+	{
+		if (i != 101 && i != 113)
+		{
+			putchar(i);
+		}
+	}
+	putchar('\n');
 	return (0);
 }
--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -10,7 +10,9 @@ int main(void)
 	int a;
 
 	for (a = 0 ; a <= 16 ; a++)
+	{
 		putchar(a);
+	}
 	putchar('\n');
 		return (0);
 }
